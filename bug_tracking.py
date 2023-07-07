64e4d173bce218cb44b8b2eb5d314976fcacbcbe
Jooa <git@jooa.xyz>
2023-07-07 22:53:23 +0200

    Style update

--- bug_tracking.py
+++ bug_tracking.py
@@ -1,14 +1,20 @@
+"""EmilyOS Bug tracking system"""
+
 class BugResolutionException(Exception):
     """Exception for bugs not resolvable by this software"""
     def __init__(self, name: str, description: str):
-        msg = self.format_message(name, description)
+        self.name = name
+        self.description = description
+        msg = self.format_message(name)
         super().__init__(msg)
 
     @staticmethod
-    def format_message(name, description):
+    def format_message(name: str):
+        """Formats the exception with a way to address it"""
         return f"Sorry we cannot help you with {name}, please consult the physical bug tracking."
 
 def bug_tracking():
+    """Bug tracking main function"""
     print("Welcome to EmilyOS digital bug tracking")
     bug_title = input("Give your bug a title: \n")
     bug_description = input("Please describe your issue: \n")
